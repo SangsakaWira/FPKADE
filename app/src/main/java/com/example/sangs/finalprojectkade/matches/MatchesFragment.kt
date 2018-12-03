d94b3061388a5548157b7053bf06664d171b5c58
Muhammad Sadewa Wicaksana W <wicaksana_irfan@yahoo.com>
2018-12-03 19:02:18 +0700

    Update Tabs Matches

--- app/src/main/java/com/example/sangs/finalprojectkade/matches/MatchesFragment.kt
+++ app/src/main/java/com/example/sangs/finalprojectkade/matches/MatchesFragment.kt
@@ -2,17 +2,15 @@ package com.example.sangs.finalprojectkade.matches
 
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.sangs.finalprojectkade.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import org.jetbrains.anko.find
 
 /**
  * A simple [Fragment] subclass.
@@ -20,12 +18,17 @@ private const val ARG_PARAM2 = "param2"
  */
 class MatchesFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_matches, container, false)
+        val view:View = inflater.inflate(R.layout.fragment_matches, container, false)
+
+        val tabs_main: TabLayout = view.find(R.id.tabs_matches)
+        val pager: ViewPager = view.find(R.id.viewpager_matches)
+
+        pager.adapter = MatchesTab(fragmentManager,context)
+        tabs_main.setupWithViewPager(pager)
+
+        return view
     }
 
 
---
+++ app/src/main/java/com/example/sangs/finalprojectkade/matches/MatchesTab.kt
@@ -0,0 +1,30 @@
+package com.example.sangs.finalprojectkade.matches
+
+import android.content.Context
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import com.example.sangs.finalprojectkade.R
+import com.example.sangs.finalprojectkade.matches.menu.LastMatchesFragment
+import com.example.sangs.finalprojectkade.matches.menu.NextMatchesFragment
+
+class MatchesTab(fm: FragmentManager?, context: Context?): FragmentStatePagerAdapter(fm) {
+    private val dataFragment = arrayOf(NextMatchesFragment(),
+        LastMatchesFragment()
+    )
+    private val dataFragmentTittle = arrayOf(context?.getString(R.string.tab_next),context?.getString(R.string.tab_last))
+
+    override fun getItem(position: Int): Fragment {
+        return dataFragment[position]
+    }
+
+    override fun getCount(): Int = dataFragment.size
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> dataFragmentTittle[position]
+            else -> dataFragmentTittle[position]
+
+        }
+    }
+}
